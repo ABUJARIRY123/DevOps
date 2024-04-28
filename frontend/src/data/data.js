809551ce0eb81ff4a1902b30179a7defe6f28b50
ABUJARIRY123 <maroridhiwani@gmail.com>
2024-04-28 21:43:27 +0300

    my portfolio 1st look

---
+++ frontend/src/data/data.js
@@ -0,0 +1,37 @@
+import WorkIm from '../assets/projects/workImg.png'
+import RealEst from '../assets/projects/realestate.png'
+import Project1 from '../assets/projects/project1.png'
+import Project2 from '../assets/projects/project2.png'
+
+export const data=[
+    {
+        id:1,
+        name:"React JS Application",
+        image:WorkIm,
+        github:"",
+        live:"",
+    },
+    {
+        id:2,
+        name:"Vanilla JS Application",
+        image:RealEst,
+        github:"",
+        live:"",
+    },
+    {
+        id:3,
+        name:"React JS Application",
+        image:Project1,
+        github:"",
+        live:"",
+    },
+    // {
+    //     id:4,
+    //     name:"React JS Application",
+    //     image:Project2,
+    //     github:"",
+    //     live:"",
+    // },
+
+
+]
