faa2fa44231e6ff3ac5beaa0e05faa1059482809
ABUJARIRY123 <maroridhiwani@gmail.com>
2024-04-29 12:24:38 +0300

    placing

--- frontend/src/components/Skills.jsx
+++ frontend/src/components/Skills.jsx
@@ -18,7 +18,7 @@ const Skills = () => {
     <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
       {/* Container */}
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center items-center w-full h-full'>
-          <div >
+          <div className='mt-40' >
               <p className='text-4xl font-bold inline border-b-4 border-lime-600'> My Tech Stacks</p>
               <p className='py-4'> These are the technologies I normally worked with to give out solutions</p>
           </div>
